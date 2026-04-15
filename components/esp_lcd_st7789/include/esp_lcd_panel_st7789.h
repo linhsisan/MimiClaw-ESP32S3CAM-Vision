f0a78487c68fb9b5ceaeb3d87168d0b65a5541d6
Linhsisan <linhsisan888@gmail.com>
2026-04-15 19:45:58 +0800

    Fix: 從 .gitignore 中移除 components，正式上傳驅動程式碼

---
+++ components/esp_lcd_st7789/include/esp_lcd_panel_st7789.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "esp_err.h"
+#include "esp_lcd_types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+esp_err_t esp_lcd_new_panel_st7789(const esp_lcd_panel_io_handle_t io, const esp_lcd_panel_dev_config_t *panel_dev_config, esp_lcd_panel_handle_t *ret_panel);
+
+#ifdef __cplusplus
+}
+#endif
